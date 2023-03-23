f6f32a36c8e34d630e7db31089c8c656922991c7
Roberto Méndez <48104122+BetoCR16@users.noreply.github.com>
2023-03-22 21:35:14 -0600

    change: slime spawning

--- src/main/java/com/edadimperial/commands/SlimeRain.java
+++ src/main/java/com/edadimperial/commands/SlimeRain.java
@@ -1,9 +1,6 @@
 package com.edadimperial.commands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +12,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SlimeRain implements CommandExecutor {
@@ -30,55 +28,72 @@ public class SlimeRain implements CommandExecutor {
         int duration = config.getInt("duration"); // Duration in seconds
         int interval = config.getInt("frequency"); // Interval in seconds
 
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.start")));
+
         List<Player> players = new ArrayList<Player>(Bukkit.getServer().getOnlinePlayers());
+        for (Player player : players) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.start")));
+        }
+
         World world = instance.getServer().getWorld("world");
+        World worldCon = instance.getServer().getWorld("imperial_build");
 
         new BukkitRunnable() {
             int count = 0;
             public void run() {
-                if (world != null) {
+                List<Player> playerList = new ArrayList<Player>(Bukkit.getServer().getOnlinePlayers());
+                System.out.println(playerList.size());
+                if (world != null && worldCon != null) {
                     world.setStorm(true);
+                    worldCon.setStorm(true);
                 }
 
                 count++;
                 if (count >= duration / interval) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.stop")));
-                    world.setStorm(false);
+                    for (Player player : playerList) {
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("messages.stop")));
+                    }
+                    if (world != null && worldCon != null) {
+                        world.setStorm(false);
+                        worldCon.setStorm(false);
+                    }
                     cancel();
                 }
 
-                for (Player player : players) {
-                    Location loc = player.getLocation();
-                    World world = loc.getWorld();
+                Collections.shuffle(playerList);
 
-                    int numPlayers = 0;
-                    for (Player otherPlayer : players) {
-                        if (otherPlayer.getLocation().distance(loc) < 10) {
-                            numPlayers++;
-                        }
-                    }
+                int numPlayers = playerList.size();
+
+
+                for (int i = 0; i <= numPlayers; i++){
+                    System.out.println(i);
+                    Location loc = playerList.get(i).getLocation();
+                    World pWorld = playerList.get(i).getWorld();
 
                     // Spawn a group of slime mobs based on the number of players in the area
                     int numSlimes = Math.min(numPlayers * 2, 50); // Limit the number of slimes to 50
-                    for (int i = 0; i < numSlimes; i++) {
-                        int x = (int) (loc.getX() + (Math.random() * 20) - 10);
-                        int y = (int) (loc.getY() + 100);
-                        int z = (int) (loc.getZ() + (Math.random() * 20) - 10);
-                        Location spawnLoc = new Location(world, x, y, z);
-                        Entity slime = world.spawnEntity(spawnLoc, EntityType.SLIME);
-                        slime.setPersistent(true);
-                        slime.setCustomName("Slime");
+                    for (int j = 0; j < numSlimes; j++) {
+                        if (pWorld.getName().equals("world") || pWorld.getName().equals("imperial_build")) {
+                            int x = (int) (loc.getX() + (Math.random() * 20));
+                            int y = (int) (loc.getY() + 100);
+                            int z = (int) (loc.getZ() + (Math.random() * 20));
+                            Location spawnLoc = new Location(pWorld, x, y, z);
+                            Entity slime = pWorld.spawnEntity(spawnLoc, EntityType.SLIME);
+                            slime.setPersistent(true);
+                            slime.setCustomName("Slime");
 
 
-                        // Schedule the entity to be removed after a certain amount of time
-                        Bukkit.getScheduler().runTaskLater(instance, new Runnable() {
-                            public void run() {
-                                slime.remove();
-                            }
-                        }, 20 * 30); // Remove the entity after 30 seconds
+                            // Schedule the entity to be removed after a certain amount of time
+                            Bukkit.getScheduler().runTaskLater(instance, new Runnable() {
+                                public void run() {
+                                    slime.remove();
+                                }
+                            }, 20 * 30); // Remove the entity after 30 seconds
+                        }
+
                     }
                 }
+
+
             }
         }.runTaskTimer(instance, 0,interval * 20);
 
